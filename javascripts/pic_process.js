a7b081b88c3bb05f9d49f889c5163904cdbb10b8
FlyingWWS <2601349541@qq.com>
2016-04-24 18:07:08 +0800

    平移 缩放  合成

--- javascripts/pic_process.js
+++ javascripts/pic_process.js
@@ -1,40 +1,35 @@
 window.onload = function(){
     var input = document.getElementById("file_input");
+    var result = document.getElementById("result");
+    var c=document.getElementById("myCanvas");
     if(typeof FileReader==='undefined'){
         result.innerHTML = "抱歉，你的浏览器不支持 FileReader";
         input.setAttribute('disabled','disabled');
     }else{
-        input.addEventListener('change',readFile,false);
+        input.addEventListener('change',function(){readFile(input,result,c,"pic")},false);
     }
 
-    var imgX = 0;
-    var imgY = 0;
-    var canvas = document.getElementById("myCanvas");
-    var context =canvas.getContext("2d");
-    canvas.onmousedown = function(event){
-        //alert("down");
-        var pos=windowToCanvas(canvas,event.clientX,event.clientY);
-        canvas.onmousemove = function(event){
-            var pos1=windowToCanvas(canvas,event.clientX,event.clientY);
-            var x=pos1.x-pos.x;
-            var y=pos1.y-pos.y;
-            pos=pos1;
-            imgX+=x;
-            imgY+=y;
-            //alert(imgX);
-            drawImage(imgX,imgY);
-        }
-        canvas.onmouseup=function(){
-            canvas.onmousemove=null;
-            canvas.onmouseup=null;
-            canvas.style.cursor="default";
-        }
+    var input_two = document.getElementById("file_input_two");
+    var result_two = document.getElementById("result_two");
+    var c_two=document.getElementById("myCanvas_two");
+    if(typeof FileReader==='undefined'){
+        result_two.innerHTML = "抱歉，你的浏览器不支持 FileReader";
+        input_two.setAttribute('disabled','disabled');
+    }else{
+        input_two.addEventListener('change',function(){
+            readFile(input_two,result_two,c_two,"pic_two");
+            document.getElementById("myCanvas_two").style.display = "inline";
+        },false);
     }
+    //移动 缩放
+    var canvas = document.getElementById("myCanvas");
+    img_move_scale(canvas,"pic");          //(画布 , div中图片的id)
+    var canvas_two = document.getElementById("myCanvas_two");
+    img_move_scale(canvas_two,"pic_two");
 }
 //
-function readFile(){
-    var result = document.getElementById("result");
-    var file = this.files[0];
+function readFile(input,result,c,id){   //(添加file的input , 放图片的div ， 画布 ， div中图片的id)
+    var file = input.files[0];
     if(!/image\/\w+/.test(file.type)){
         alert("文件必须为图片！");
         return false;
@@ -42,12 +37,16 @@ function readFile(){
     var reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = function(e){
-        result.innerHTML = '<img src="'+this.result+'" id="pic" alt=""/>'
-        var c=document.getElementById("myCanvas");
+        result.innerHTML = '<img src="'+this.result+'" id="'+id+'" alt=""/>'
+        //var c=document.getElementById("myCanvas");
         var ctx=c.getContext("2d");
-        ctx.save();
-        var img=document.getElementById("pic");
+        var img=document.getElementById(id);
+        //c.width = img.width;
+        //c.height = img.height;
         ctx.drawImage(img,0,0);
+        //var c_two=document.getElementById("myCanvas_two");
+        //var ctx_two=c_two.getContext("2d");
+        //ctx_two.drawImage(img,0,0);
     }
 }
 function convertCanvasToImage() {
@@ -56,8 +55,9 @@ function convertCanvasToImage() {
 //            var image = new Image();
 //            image.src = canvas.toDataURL("image/png");
     var src = canvas.toDataURL("image/png");
-    result.innerHTML = '<img src="'+src+'" id="pic" alt=""/>'
+    result.innerHTML = '<img src="'+src+'" id="pic" alt=""/>';
     link_img.innerHTML = "<a id='download_link' target='_blank' href='"+src+"'>下载</a>";
+    document.getElementById("myCanvas_two").style.display = "none"; //转成图片后去掉第二个canvas
     //return image;
 }
 
@@ -103,7 +103,45 @@ function oppositeColor(){
     ctx.putImageData(imgData,0,0);
 }
 
-//
+//图片移动 缩放
+function img_move_scale(canvas,id){     //(画布 , div中图片的id)
+    var imgX = 0;
+    var imgY = 0;
+    var imgScale = 1;   //缩放比例
+    canvas.onmousedown = function(event){       //图片移动
+        var pos=windowToCanvas(canvas,event.clientX,event.clientY);
+        canvas.onmousemove = function(event){
+            canvas.style.cursor="move";
+            var pos1=windowToCanvas(canvas,event.clientX,event.clientY);
+            var x=pos1.x-pos.x;
+            var y=pos1.y-pos.y;
+            pos=pos1;
+            imgX+=x;
+            imgY+=y;
+            //alert(imgX);
+            drawImage(canvas,imgX,imgY,imgScale,id);
+        }
+        canvas.onmouseup=function(){
+            canvas.onmousemove=null;
+            canvas.onmouseup=null;
+            canvas.style.cursor="default";
+        }
+    }
+    canvas.onmousewheel=canvas.onwheel=function(event){//图片缩放
+        var pos=windowToCanvas(canvas,event.clientX,event.clientY);
+        event.wheelDelta=event.wheelDelta?event.wheelDelta:(event.deltaY*(-40));
+        if(event.wheelDelta>0){
+            imgScale*=2;
+            imgX=imgX*2-pos.x;
+            imgY=imgY*2-pos.y;
+        }else{
+            imgScale/=2;
+            imgX=imgX*0.5+pos.x*0.5;
+            imgY=imgY*0.5+pos.y*0.5;
+        }
+        drawImage(canvas,imgX,imgY,imgScale,id);
+    }
+}
 function windowToCanvas(canvas,x,y) {
     var bbox = canvas.getBoundingClientRect();
     return {
@@ -111,14 +149,25 @@ function windowToCanvas(canvas,x,y) {
         y: y - bbox.top - (bbox.height - canvas.height) / 2
     };
 }
-function drawImage(imgX,imgY){
-    var canvas = document.getElementById("myCanvas");
+function drawImage(canvas,imgX,imgY,imgScale,id){
     var context =canvas.getContext("2d");
-    var img=document.getElementById("pic");
+    var img=document.getElementById(id);
     //alert(img.width);
     //alert(imgX);
     context.clearRect(0,0,canvas.width,canvas.height);
-    context.drawImage(img,0,0,img.width,img.height,imgX,imgY,img.width,img.height);
+    context.drawImage(img,0,0,img.width,img.height,imgX,imgY,img.width*imgScale,img.height*imgScale);
 }
 
+//合成
+function mixup(){
+    var c = document.getElementById("myCanvas");
+    var ctx =c.getContext("2d");
 
+    var c_two=document.getElementById("myCanvas_two");
+    var result_two = document.getElementById("result_two");
+    var src = c_two.toDataURL("image/png");
+    result_two.innerHTML = '<img src="'+src+'" id="pic_two" alt=""/>';
+    var pic_two = document.getElementById("pic_two");
+    ctx.drawImage(pic_two,0,0);
+
+}
